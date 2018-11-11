3284ecc42f091b87214c0771e2442cf559b47b68
dzipet <dziugas.petkevicius@gmail.com>
2018-11-11 15:35:01 +0200

    Initial empty projects

---
+++ HomeWork.Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeWork.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
